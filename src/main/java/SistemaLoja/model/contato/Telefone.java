91b7d2c54d2fe285e69a3c9e3a17021d75ea25e7
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-09-28 11:46:41 -0300

    Tabela Telefone adicionada.

---
+++ src/main/java/SistemaLoja/model/contato/Telefone.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SistemaLoja.model.contato;
+
+/**
+ *
+ * @author pedroh
+ */
+public class Telefone {
+    private int idTelefone;
+    private String telefone;
+
+    public int getIdTelefone() {
+        return idTelefone;
+    }
+
+    public void setIdTelefone(int idTelefone) {
+        this.idTelefone = idTelefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Telefone(int idTelefone, String telefone) {
+        this.setIdTelefone(idTelefone);
+        this.setTelefone(telefone);
+    }
+    
+}
