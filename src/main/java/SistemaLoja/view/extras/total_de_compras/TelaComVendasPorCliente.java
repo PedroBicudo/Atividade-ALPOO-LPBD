5f4d9366f93435c371c9c8b640781127aa487f5e
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-10-06 14:16:54 -0300

    Arquivo renomeado.

--- src/main/java/SistemaLoja/view/extras/total_de_compras/TelaComVendasPorCliente.java
+++ src/main/java/SistemaLoja/view/extras/total_de_compras/TelaComVendasPorCliente.java
@@ -17,7 +17,7 @@ import javax.swing.JPanel;
  *
  * @author pedroh
  */
-public class ListaDeComprasPorCliente extends javax.swing.JFrame {
+public class TelaComVendasPorCliente extends javax.swing.JFrame {
     
     private ArrayList<ClienteEVenda> vendasPorCliente;
     private JPanel panelContainer;
@@ -26,7 +26,7 @@ public class ListaDeComprasPorCliente extends javax.swing.JFrame {
     /**
      * Creates new form ListaDeComprasPorCliente
      */
-    public ListaDeComprasPorCliente() {
+    public TelaComVendasPorCliente() {
         database = SQLServerDao.getInstance();
         initComponents();
         initComponentsExtras();
@@ -111,20 +111,21 @@ public class ListaDeComprasPorCliente extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ListaDeComprasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaComVendasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ListaDeComprasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaComVendasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ListaDeComprasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaComVendasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ListaDeComprasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaComVendasPorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ListaDeComprasPorCliente().setVisible(true);
+                new TelaComVendasPorCliente().setVisible(true);
             }
         });
     }
